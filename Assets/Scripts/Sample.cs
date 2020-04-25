af50965aa830392e761e575950660274d54e2056
BlueKoopa0127 <bluekoopa@me.com>
2020-04-25 19:59:40 +0900

    コンフリクトw

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -7,7 +7,7 @@ public class Sample : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //ahahahahahaaaaa
     }
 
     // Update is called once per frame
