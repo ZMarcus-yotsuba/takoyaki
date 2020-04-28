0c4cb667f544ffa9b9ebf108cba86c5df98ccd1f
BlueKoopa0127 <bluekoopa@me.com>
2020-04-28 15:36:21 +0900

    たこ焼きゲーム（仮）

---
+++ Assets/Scripts/Component/TakoyakiComponent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TakoyakiGame.Main
+{
+    public class TakoyakiComponent : MonoBehaviour
+    {
+        //落ちるスピード、constは定数の宣言
+        private const float SPEED = 0.01f;
+
+        //落ちているかのフラグ
+        private bool falling;
+
+        //生成された時
+        private void Start()
+        {
+            //落ちる状態にする
+            falling = true;
+        }
+
+        private void Update()
+        {
+            //もし落ちている状態ならば
+            if (falling)
+            {
+                //毎フレームFallメソッドを呼び出す
+                Fall();
+            }
+        }
+
+        //落ちるメソッド
+        public void Fall()
+        {
+            //現在の座標を取得
+            Vector2 pos = transform.position;
+
+            //Y座標からSPEEDを引く
+            pos.y -= SPEED;
+
+            //引いたものを現在の座標にする
+            transform.position = pos;
+
+            //つまり毎フレームY座標からSPEED分引かれていく仕組み
+        }
+    }
+}
---
+++ Assets/Scripts/Single/TakoyakiManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TakoyakiGame.Main
+{
+    public class TakoyakiManager : MonoBehaviour
+    {
+        //Prefabのたこ焼きオブジェクトを格納
+        [SerializeField]
+        private GameObject takoyakiObject = null;
+
+        //毎フレーム呼び出される
+        private void Update()
+        {
+            //もしエンターキーが押されたら
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                //CreateTakoyakiメソッドを呼び出す
+                CreateTakoyaki();
+            }
+        }
+
+        //たこ焼きを生成するメソッド
+        private void CreateTakoyaki()
+        {
+            //Instantiateでオブジェクトの生成、これ大事
+            Instantiate(takoyakiObject, transform);
+        }
+    }
+}
